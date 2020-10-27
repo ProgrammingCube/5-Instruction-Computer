7bce83329502ac3d5154f81e4b1b7755dba382fd
ProgrammingCube <gindiamondandcubes@gmail.com>
2020-10-27 17:23:42 -0400

    First upload

---
+++ main.py
@@ -0,0 +1,155 @@
+import sys, os, time, math
+from PyQt5 import uic, QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QTimer, pyqtSlot
+
+form_class = uic.loadUiType("main.ui")[0]
+register_set = set('abcdefxy')
+registers = 'abcdefgxy'
+
+err = False
+
+def parseCode(self):
+    self.codeStatus.setText("Running...")
+    pc = 0
+    lines = self.codeEdit.document().blockCount()
+    boxList = []
+    regText = []
+    regText.append(self.registerAValue)
+    regText.append(self.registerBValue)
+    regText.append(self.registerCValue)
+    regText.append(self.registerDValue)
+    regText.append(self.registerEValue)
+    regText.append(self.registerFValue)
+    regText.append(self.registerXValue)
+    regText.append(self.registerYValue)
+
+    boxList.append(self.incBox)
+    boxList.append(self.decBox)
+    boxList.append(self.iszBox)
+    boxList.append(self.jmpBox)
+    boxList.append(self.stpBox)
+    while (pc < lines):
+        currentLine = self.codeEdit.document().findBlockByLineNumber(pc).text()
+        currentLine = currentLine.lstrip()  #remove leading whitespaces
+        if currentLine[0] == ';':
+            pc += 1
+            continue
+        tokens = currentLine.split()
+        #print(currentLine)
+        #parse the opcode.
+        #because it is like only 5 instructions,
+        #I'm not sweating good coding practice here.
+        if tokens[0] ==  "inc":
+            try:
+                reg = registers.index(tokens[1])
+            except:
+                self.codeStatus.setText('NONEXISTANT REGISTER')
+                err = True
+                break
+            tempreg = regText[reg].text()
+            try:
+                tempreg = int(tempreg)
+            except:
+                self.codeStatus.setText('NONINT IN REGISTER')
+                err = True
+                break
+            tempreg = tempreg + 1
+            regText[reg].setText(str(tempreg))
+            pc += 1
+
+        elif tokens[0] ==  "dec":
+            try:
+                reg = registers.index(tokens[1])
+            except:
+                self.codeStatus.setText('NONEXISTANT REGISTER')
+                err = True
+                break
+            tempreg = regText[reg].text()
+            try:
+                tempreg = int(tempreg)
+            except:
+                self.codeStatus.setText('NONINT IN REGISTER')
+                err = True
+                break
+            tempreg = tempreg - 1
+            if tempreg < 0:
+                self.codeStatus.setText('NEGATIVE REGISTER')
+                err = True
+                break
+            regText[reg].setText(str(tempreg))
+            pc += 1
+        
+        elif tokens[0] ==  "jmp":
+            try:
+                pc = int(tokens[1])
+            except:
+                self.codeStatus.setText('NONINT PC')
+                err = True
+                break
+
+        elif tokens[0] ==  "isz":
+            try:
+                reg = registers.index(tokens[1])
+            except:
+                self.codeStatus.setText('NONEXISTANT REGISTER')
+                err = True
+                break
+            regTest = regText[reg].text()
+            try:
+                testReg = int(regTest)
+            except:
+                self.codeStatus.setText('NONINT IN REGISTER')
+                err = True
+                break
+            if testReg == 0:
+                pc += 2
+            else:
+                pc += 1
+
+        elif tokens[0] ==  "stp":
+            break
+
+        self.instructionBox.setText(tokens[0] + ' ' + tokens[1])
+        QApplication.processEvents()
+        #time.sleep(0.1)
+        time.sleep(int(self.currentSpeed.text())/1000)
+
+class mainWindow(QtWidgets.QMainWindow, form_class):
+    def __init__(self, parent=None):
+        QtWidgets.QMainWindow.__init__(self, parent)
+        self.setupUi(self)
+
+        self.runButton.clicked.connect(self.start_clicked)
+        self.stopButton.clicked.connect(self.stop_clicked)
+        self.stepButton.clicked.connect(self.step_clicked)
+        self.speedSlider.setSingleStep(10)
+        self.speedSlider.valueChanged[int].connect(self.speedChange)
+
+        self.initUI()
+    
+    def initUI(self):
+        #self.statusBar().showMessage('When you : bottom text')
+        self.show()
+    
+    @pyqtSlot()
+    def start_clicked(self):
+        print('Running...')
+        parseCode(self)
+        if err == True:
+            self.codeStatus.setText("Done!")
+    
+    def stop_clicked(self):
+        print('stopping code...')
+    
+    def step_clicked(self):
+        print('Stepping...')
+    
+    def speedChange(self, value):
+        self.currentSpeed.setText(str(value))
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex  = mainWindow()
+    sys.exit(app.exec_())
